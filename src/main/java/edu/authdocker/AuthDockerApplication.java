fb801b3ae5f9b5ba34ec6414c3a8c8962d36a33f
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-07-19 22:16:54 -0300

    Criação do projeto

---
+++ src/main/java/edu/authdocker/AuthDockerApplication.java
@@ -0,0 +1,13 @@
+package edu.authdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/authdocker/AuthDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.authdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

abe5506f53ba3251d62f1025a09aafa3fd759a2b
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-07-21 15:18:35 -0300

    Organização diretorios

---
+++ database/usuarios_docker_database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS usuarios_docker;
+USE usuarios_docker;
+
+CREATE TABLE usuario_table(
+	id INT PRIMARY KEY NOT NULL,
+    nome_usuario VARCHAR(75) NOT NULL UNIQUE,
+    email_usuario VARCHAR(100) NOT NULL UNIQUE,
+    password_usuario VARCHAR(250) NOT NULL,
+    url_profile_picture VARCHAR(300) UNIQUE
+);
